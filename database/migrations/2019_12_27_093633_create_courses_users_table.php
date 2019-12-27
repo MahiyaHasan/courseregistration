571dde0e441bb991e67ce23acca8fc262a835bca
Mahiya Hasan <mahiya.hasan.03@gmail.com>
2019-12-27 16:21:09 +0600

    initial commit

---
+++ database/migrations/2019_12_27_093633_create_courses_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->Integer('user_id');
+            $table->Integer('course_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses_users');
+    }
+}
